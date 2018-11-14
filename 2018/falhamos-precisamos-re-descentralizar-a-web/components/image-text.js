03d662b14d1d55a390096863cec2841cf1eea2e9
Jaydson Gomes <jayalemao@gmail.com>
2018-11-14 17:46:47 -0200

    adding slides

--- 2018/falhamos-precisamos-re-descentralizar-a-web/components/image-text.js
+++ 2018/falhamos-precisamos-re-descentralizar-a-web/components/image-text.js
@@ -1,27 +1,23 @@
 import React, {Fragment} from 'react'
 import { Image } from 'mdx-deck'
 
-export default class Cover extends React.Component {
+export default class ImageText extends React.Component {
   render () {
     return (
       <Fragment>
         <Image src={this.props.imgSrc} />
         <div style={{
           position: 'absolute',
-          top: 0,
-          fontSize: '4em',
-          display: 'flex',
-          alignItems: 'center',
-          alignContent: 'center',
+          bottom: 40,
+          fontSize: '2em',
           color: '#fff',
           width: '100%',
-          height: '90%',
           textShadow: '6px 3px 11px #000',
           padding: '20px',
           fontFamily: 'DejaVu Sans',
           lineHeight: '1.3',
           fontWeight: '700'
-        }}>{this.props.title}<br/>{this.props.content}</div>
+        }}>{this.props.content}</div>
       </Fragment>
     )
   }
